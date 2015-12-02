9703fb239071a16251013e5825806e3554aab841
Hugh <yujun_daxia@163.com>
2015-12-02 18:08:42 +0800

    day 1

--- packages/cqa-posting/lib/CQA_parameters.js
+++ packages/cqa-posting/lib/CQA_parameters.js
@@ -3,7 +3,7 @@ CQAPostings.parameters = {};
 CQAPostings.parameters.get = function (termArgu) {
 	check(termArgu, Match.Any);
 
-	var paraInitals = CommunityQA.utils.deepExtend(true, {}, CQAPostings.views.baseParameters);
+	var paraInitials = CommunityQA.utils.deepExtend(true, {}, CQAPostings.views.baseParameters);
 	paraInitials = CommunityQA.callbacks.run("paramsForPostings", paraInitials, termArgu);
 
 	if (_.isEmpty(paraInitials.options.sort)) {
@@ -48,13 +48,37 @@ function addParametersOfTimes (paraInitials, termArgu) {
 CommunityQA.callbacks.add("paramsForPostings", addParametersOfTimes);
 
 function limitNumberOfPostings (paraInitials, termArgu) {
-	var 
+	var  maxNumber = 200;
+	if (typeof termArgu.limit !== 'undefined') {
+		_.extend(paraInitials.options, {limit: parseInt(termArgu.limit)});
+	}
+
+	if (!paraInitials.options.limit || paraInitials.options.limit === 0 || paraInitials.options.limit > maxNumber) {
+		paraInitials.options.limit = maxNumber;
+	}
+
+	return paraInitials;
 
 }
 
 CommunityQA.callbacks.add("paramsForPostings", limitNumberOfPostings);
 
 function concealPostingsOfFuture (paraInitials, termArgu) {
-	
+	var withinAnHour = moment().add(1, "hour").toDate();
+
+	if (!paraInitials.showFuture) {
+		if (!!paraInitials.find.postedDate) {
+			if (!!paraInitials.find.postedDate.$lt) {
+				var lt = paraInitials.find.postedDate.$lt;
+				paraInitials.find.postedDate.$lt = lt < withinAnHour ? lt : withinAnHour;
+			} else {
+				paraInitials.find.postedDate.$lt = withinAnHour;
+			}
+		} else {
+			paraInitials.find.postedDate = {$lt: withinAnHour};
+		}
+	}
+
+	return paraInitials;
 }
 CommunityQA.callbacks.add("paramsForPostings", concealPostingsOfFuture);
